ad353142f6d14ada97c72666f610838ce448a5f8
suyog ojha <ojhasuyog2020@gmail.com>
2022-04-08 13:28:16 +0545

    sd

--- project/settings.py
+++ project/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -144,7 +145,7 @@ MEDIA_ROOT = ""
 
 
 
-STATICFILES_DIRS = [BASE_DIR / "static"]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 
 MATERIAL_ADMIN_SITE = {
